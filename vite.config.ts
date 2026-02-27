70b644a50fa98694ecd2be284897a84d5ae42061
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2026-02-28 01:51:18 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/frontend/',
   resolve: {
     alias: {
