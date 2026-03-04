adf9a70368d0db55ff127119e2237d642b63c609
rLukoyanov <rulukoyanov@gmail.com>
2026-03-04 17:44:14 +0400

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,7 @@ const RubricsScreen = lazy(() => import('./pages/Rubrics/RubricsScreen'));
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-[#f3f4f6] dark:bg-[#0d0d0d] text-slate-900 dark:text-[#fafafa]">
-        <Suspense fallback={null}>
-          <Routes>
-            <Route path="/" element={<MapScreen />} />
-            <Route path="/rubrics" element={<RubricsScreen />} />
-          </Routes>
-        </Suspense>
-      </div>
-    </BrowserRouter>
+    <MapScreen />
   );
 }
 
