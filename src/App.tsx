e69ab5a7063d04262d4caf70956e2b90fef5537e
rLukoyanov <rulukoyanov@gmail.com>
2026-03-04 17:45:04 +0400

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
 import './index.css';
 
 const MapScreen = lazy(() => import('./pages/Map/MapScreen'));
-const RubricsScreen = lazy(() => import('./pages/Rubrics/RubricsScreen'));
 
 function App() {
   return (
