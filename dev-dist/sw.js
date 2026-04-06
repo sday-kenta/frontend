d084710eb970d9dd3f0dd97b719c98e69b3c0bdf
unknown <8bit.1994@mail.ru>
2026-04-06 23:35:55 +0300

    fix: отображение аватарок

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/frontend/index.html",
-    "revision": "0.h225qf1kqes"
+    "revision": "0.ravrg2pfftk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/frontend/index.html"), {
